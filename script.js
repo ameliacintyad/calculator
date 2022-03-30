cc5b97cfafa8377041a5a66020fc69acfb3d1e55
Amelia Cintya Dewi <100978393+ameliacintyad@users.noreply.github.com>
2022-03-30 16:19:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
+/* output screen */
 let outputscreen = document.getElementById("output-screen")
 
+/* display screen */
 function display(num) {
     outputscreen.value += num
 }
 
+/* calculator */
 function calculator() {
     try {
         outputscreen.value = eval(outputscreen.value)
     }
+    /* muncul invalid jika memasukan operator lebih dari 1 dan klik tombol = */
     catch (err) {
         alert("invalid")
     }
 }
 
+/* clr screen */
 function bersihkan() {
     outputscreen.value = ""
 }
 
+/* del 1 angka */
 function del() {
     outputscreen.value = outputscreen.value.slice(0, -1)
 }
